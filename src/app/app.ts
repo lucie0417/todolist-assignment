11e773b12e641bdb76932aeec99aa5c20936d3e0
Lucie <ipeicram0417@gmail.com>
2026-03-19 22:23:10 +0800

    Function completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -8,5 +14,52 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todolist-assignment');
+  todos = signal<Todo[]>(this.readFromStorage());
+  title = signal("");
+
+  addTodo() {
+    const text = this.title().trim();
+    if (!text) return;
+
+    const newTodo: Todo = {
+      id: crypto.randomUUID() || Date.now().toString(),
+      title: text,
+      completed: false,
+    }
+
+    this.todos.update(list => [newTodo, ...list]);
+    this.title.set('');
+    this.saveToStorage();
+  }
+
+  toggleTodo(id: string) {
+    this.todos.update((list => list.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+    this.saveToStorage();
+  }
+
+  remove(id: string) {
+    this.todos.update((list => list.filter(todo => todo.id !== id)));
+    this.saveToStorage();
+  }
+
+  // save to localStorage
+  readFromStorage(): Todo[] {
+    try {
+      return JSON.parse(localStorage.getItem('todos') || '[]');
+    } catch {
+      return [];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos()));
+  }
+
+  // IME composing
+  onKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter' && !event.shiftKey && !event.isComposing) {
+      event.preventDefault();
+      this.addTodo();
+    }
+  }
 }
